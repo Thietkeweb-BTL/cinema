2ca84dfa6c957d45e8290bb1421ff0c8f41dffe5
HuyenHuyencodoicocap <142354967+HuyenHuyencodoicocap@users.noreply.github.com>
2023-10-21 01:15:23 +0700

    BTL

--- trang-chu/main.js
+++ trang-chu/main.js
@@ -1,1 +1,12 @@
+function resizeIFrameToFitContent( iFrame ) {
+}
+function resizeIfame() {
+    var iframes = document.querySelectorAll("iframe");
+        
+    iframes[0].style.height = iframes[0].contentWindow.document.body.scrollHeight+"px";
+}
 
+window.addEventListener('DOMContentLoaded', function(e) {
+    // or, to resize all iframes:
+    resizeIfame();
+} );

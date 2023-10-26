0542b7d80613efe652fd7bc74a9804118650af74
HuyenHuyencodoicocap <1632004huyen@gmail.com>
2023-10-26 23:47:48 +0700

    BTL

--- resources/js/trangchu.js
+++ resources/js/trangchu.js
@@ -1,13 +1,15 @@
 function resizeIFrameToFitContent(iFrame) {
 }
 function resizeIfame() {
-    var iframes = document.querySelectorAll("iframe");
+    var header = document.getElementById("header");
+    var footer = document.getElementById("footer");
 
-    iframes[0].style.height = iframes[0].contentWindow.document.body.scrollHeight + "px";
+    header.style.height = header.contentWindow.document.body.scrollHeight + "px";
+    footer.style.height = footer.contentWindow.document.body.scrollHeight + "px";
 }
 
 window.addEventListener('DOMContentLoaded', function (e) {
     // or, to resize all iframes:
-    this.document.body.setAttribute("onresize") = "resizeIfame()"
+    this.document.body.setAttribute("onresize","resizeIfame()");
     resizeIfame();
 });
